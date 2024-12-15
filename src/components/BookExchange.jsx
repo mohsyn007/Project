c16af3e6b50db0670a748180a021383cc8065b75
mohsyn007 <mohsinalam1141@gmail.com>
2024-12-15 10:56:14 +0600

    added something

--- src/components/BookExchange.jsx
+++ src/components/BookExchange.jsx
@@ -3,7 +3,77 @@ import React from 'react';
 const BookExchange = () => {
     return (
         <div>
-            Exchange
+            Exchange <div className="hero bg-base-200 min-h-screen w-full">
+        <div className=" flex-col lg:flex-row-reverse">
+          
+          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <form className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">BooK Name</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Seller name"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Book Category</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Number"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+             
+             
+            
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">About book</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Seller name"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Book  url</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Seller name"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Wanted Book</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Book name"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+             
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Submit</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
         </div>
     );
 };
